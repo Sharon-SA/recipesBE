21ebcd10dec8822aa24cd0e893a9242b94a7a346
Sharon-SA <ssotunde-adesina@unomaha.edu>
2022-11-13 15:25:36 -0600

    commit

--- recipes_rest/settings.py
+++ recipes_rest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0l51udbjw89ybzik%-otu)31$kzxsx(o%!w)%idksk96si(drb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
